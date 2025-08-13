c7770f74af129fa62e84c3f7a590338b6c944dd5
DANIEL MUELAS <danmcode@gmail.com>
2025-08-12 22:09:04 -0500

    chore: config .env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env.model';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService<Env>,
+  ) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const myVar = this.configService.get('MY_VAR');
+    const hello = this.appService.getHello();
+    return `${hello} - ${myVar}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule { }
