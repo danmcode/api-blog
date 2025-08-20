9fde6764d7ce8a8fbfd4c39d9be806beb2ab7fa9
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-08-19 20:08:44 -0500

    Implement Categories module: Add CategoriesController, CategoriesService, and related DTOs
    - Created CategoriesController for handling CRUD operations on categories.
    - Developed CategoriesService for business logic related to categories, including error handling.
    - Introduced CreateCategoryDto and UpdateCategoryDto for category data validation.
    - Defined Category entity for database schema representation.
    - Updated PostsModule to include categories functionality and establish relationships.

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,11 +3,14 @@ import { PostsService } from './services/posts.service';
 import { PostsController } from './ controllers/posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { Category } from './entities/category.entity';
+import { CategoriesController } from './ controllers/categories.controller';
+import { CategoriesService } from './services/categories.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
-  controllers: [PostsController],
-  providers: [PostsService],
-  exports: [PostsService]
+  imports: [TypeOrmModule.forFeature([Post, Category])],
+  controllers: [PostsController, CategoriesController],
+  providers: [PostsService, CategoriesService],
+  exports: [PostsService, CategoriesService]
 })
 export class PostsModule { }
