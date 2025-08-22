957b3980202a98f0d0e4a6ee44eaad01a4b42b75
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-08-21 23:45:35 -0500

    feature(auth.module): Adding auth module with passport

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.staregy';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy]
+})
+export class AuthModule { }
