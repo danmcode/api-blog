827db33c1ca55356cd226c2cf0bc69d5d024f465
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-08-22 20:28:42 -0500

    feat: creating a auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './services/auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategies/local.staregy';
 import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule { }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,9 @@
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToOne, JoinColumn, OneToMany, BeforeInsert } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, JoinColumn, OneToOne, OneToMany, BeforeInsert } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { Exclude } from 'class-transformer';
+
 import { Profile } from './profile.entity';
 import { Post } from '../../posts/entities/post.entity';
-import * as bcrypt from 'bcrypt';
 
 @Entity({
     name: 'users',
@@ -10,41 +12,29 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({
-        type: 'varchar',
-        length: 255,
-        unique: true
-    })
+    @Column({ type: 'varchar', length: 255, unique: true })
     email: string;
 
-    @Column({
-        type: 'varchar',
-        length: 255,
-        select: false
-    })
+    @Exclude()
+    @Column({ type: 'varchar', length: 255 })
     password: string;
 
     @CreateDateColumn({
         type: 'timestamptz',
         default: () => 'CURRENT_TIMESTAMP',
-        name: 'created_at'
+        name: 'created_at',
     })
     createdAt: Date;
 
     @UpdateDateColumn({
         type: 'timestamptz',
         default: () => 'CURRENT_TIMESTAMP',
-        name: 'updated_at'
+        name: 'updated_at',
     })
     updatedAt: Date;
 
-    @OneToOne(() => Profile, {
-        nullable: false,
-        cascade: true
-    })
-    @JoinColumn({
-        name: 'profile_id'
-    })
+    @OneToOne(() => Profile, { nullable: false, cascade: true })
+    @JoinColumn({ name: 'profile_id' })
     profile: Profile;
 
     @OneToMany(() => Post, (post) => post.user)
